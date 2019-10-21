4eaf14e2726de43b89ae4c59edbbe8a1d756ede5
AntsErik <ants.erik.noormagi@aweroc.com>
2019-10-21 11:57:41 +0300

    Initial push.

---
+++ src/ee/praktika/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,123 @@
+package ee.praktika.aopdemo.aspect;
+
+import java.util.List;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import ee.praktika.aopdemo.Account;
+
+@Aspect
+@Component
+@Order( 4 )
+public class MyDemoLoggingAspect {
+
+    @Around( "execution(* ee.praktika.aopdemo.service.*.getFortune(..))" )
+    public Object aroundGetFortune( ProceedingJoinPoint theProceedingJoinPoint ) throws Throwable{
+
+        //print out which method are we advising on
+        String method = theProceedingJoinPoint.getSignature().toShortString();
+        System.out.println( "\n======>>> Executing @Around on method: " + method );
+
+        //get the beggining timestamp
+        long begin = System.currentTimeMillis();
+
+        //excecute the method
+        Object result = theProceedingJoinPoint.proceed(); //handle theProceedingJoinPoint and target proceed() to excecute
+
+        //get the ending timestamp
+        long end = System.currentTimeMillis();
+
+        //display the duration for the method to excecute
+        long duration = end - begin;
+        System.out.println( "\n========>>> Duration: " + duration / 1000.0 + " seconds." ); //translating milliseconds to seconds
+
+        return result;
+
+    }
+
+    @After( "execution(* ee.praktika.aopdemo.dao.AccountDAO.findAccounts(..))" )
+    public void afterFinallyFindAccountsAdvice( JoinPoint theJoinPoint ){
+
+        //print out which method are we advising on
+        String method = theJoinPoint.getSignature().toShortString();
+        System.out.println( "\n======>>> Executing @After (finally) on method: " + method );
+    }
+
+    @AfterThrowing( pointcut = "execution(* ee.praktika.aopdemo.dao.AccountDAO.findAccounts(..))", throwing = "theExc" )
+    public void afterThrowingFindAccountsAdvice( JoinPoint theJoinPoint, Throwable theExc ){
+
+        //print out which method we are advising on
+        String method = theJoinPoint.getSignature().toShortString();
+        System.out.println( "\n======>>> Executing @AfterThrowing on method: " + method );
+
+        //log the exception
+        System.out.println( "\n======>>> Exceptiong is: " + theExc );
+    }
+
+    @AfterReturning( pointcut = "execution(* ee.praktika.aopdemo.dao.AccountDAO.findAccounts(..))", returning = "result" )
+    public void afterReturningFindAccountsAdvice(
+        JoinPoint theJoinPoint, List<Account> result ){
+
+        //print out which method are we advising on
+        String method = theJoinPoint.getSignature().toShortString();
+        System.out.println( "\n======>>> Executing @AfterReturning on method: " + method );
+
+        //print out the result of the method call
+        System.out.println( "\n======>>> result is: " + result );
+
+        //watn to post-process the data and modify it, before it makes back to caller
+
+        //convert the account name to all uppercase
+        convertAccountNamesToUpperCase( result );
+
+        System.out.println( "\n======>>> result is: " + result );
+
+    }
+
+    private void convertAccountNamesToUpperCase( List<Account> result ){
+
+        //loop through accounts
+        for( Account tempAccount : result ) {
+
+            //get uppercase version of names
+            String theUpperName = tempAccount.getName().toUpperCase();
+
+            //update the name on the account object
+            tempAccount.setName( theUpperName );
+
+        }
+    }
+
+    @Before( "ee.praktika.aopdemo.aspect.AopExpressions.referencePointcutIgnoreGetSet()" )
+    public void beforeAddAccountAdvice( JoinPoint theJoinPoint ){
+        System.out.println( "\n======>>> Executing @Before advice on addAccount() in the DAO package" );
+
+        //display the method signature
+        MethodSignature methodSignature = (MethodSignature)theJoinPoint.getSignature();
+
+        System.out.println( "Method: " + methodSignature );
+
+        //display the method arguments that are being passed in
+
+        //get the arguments
+        Object[] args = theJoinPoint.getArgs();
+
+        //looping through the arguments and printing them out
+        for( Object tempArg : args ) {
+            System.out.println( tempArg );
+
+            if( tempArg instanceof Account ) {
+                //downcast and print Account specific stuff
+                Account theAccount = (Account)tempArg;
+
+                System.out.println( "Account name: " + theAccount.getName() );
+                System.out.println( "Level name: " + theAccount.getLevel() );
+            }
+        }
+    }
+}
